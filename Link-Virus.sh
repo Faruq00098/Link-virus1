2032b9f487940d15b2fd66a7d77362f49bbfe856
Faruq00098 <155282497+Faruq00098@users.noreply.github.com>
2024-07-23 20:48:38 +0700

    Add files via upload

---
+++ Link-Virus.sh
@@ -0,0 +1,64 @@
+#!/system/bin/bash
+
+# by [FARUQ]
+
+# pilihan
+
+e=echo
+s=sleep
+h=https
+b=bit.ly
+k=3ild93L
+
+# pembersih teks
+c=clear
+$e #pembersih
+
+clear
+
+# tampilan
+$s 2
+figlet Link-Virus
+$s 1
+
+            $e "           pilih"
+            $e "            ↓↓"
+
+      $e "======================================"
+      $e "(1) Link-Virus"
+      $e "(2) keluar    "
+      $e "======================================"
+
+
+    read -p  " pilih 1/2 : " pil
+          if [ $spil  "1" ]
+           then
+
+echo  "sabar."
+sleep 1
+ echo "sabar.."
+ sleep 1
+  echo "sabar..."
+      sleep 2
+
+ echo "ini"
+echo  "↓↓↓"
+  echo " ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+  echo " $h://$b/$k"
+  echo " ÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷"
+
+read   -p  "   apakah anda ingin keluar y : " pilll
+  elif [ $spilll "y" ]
+  then
+  clear
+
+ elif [ $spil "2" ]
+  then
+  echo "keluar dari program"
+  sleep 3
+echo "exit"
+ else
+ echo " tidak ada dalam list"
+
+  echo "mohon coba lagi"
+fi
